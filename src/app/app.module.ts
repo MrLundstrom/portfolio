811e7af31e81d52b13896327a1d5e725d6d658c9
Erik Lundström <erik.andreas.lundstrom@gmail.com>
2020-01-14 12:02:39 +0100

    added skills-page, added css for containers, pages and columns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import {  } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin,faGithub } from '@fortawesome/free-brands-svg-icons';
-import {  } from '@fortawesome/free-regular-svg-icons';
+import { faFire } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub, faAngular, faBehance, faHtml5, faJs, faSass, faCss3Alt } from '@fortawesome/free-brands-svg-icons';
+import { } from '@fortawesome/free-regular-svg-icons';
 
 
 import { AppComponent } from './app.component';
 import { SplashComponent } from './components/splash/splash.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContentComponent } from './components/content/content.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashComponent,
     NavbarComponent,
-    ContentComponent
+    ContentComponent,
+    SkillsComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +34,7 @@ export class AppModule {
   constructor(library: FaIconLibrary) {
     // Add an icon to the library for convenient access in other components
     library.addIcons(
-      faLinkedin, faGithub
+      faLinkedin, faGithub, faAngular, faBehance, faHtml5, faJs, faSass, faFire, faCss3Alt
     );
   }
- }
+}
