89e9699491ad39bc5a4869e0a71edf5b0499d749
Erik Lundstrom <erik.andreas.lundstrom@gmail.com>
2020-10-25 14:02:06 +0100

    changed so that the hover text is always visible on mobie for the icon links, and some minors changes to make the site easier with touch devices

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -13,7 +13,7 @@ export class TimelineComponent implements OnInit {
     { year: 2008, description: "Finnished school and started to attend upper secondary school at Mikael Elias Teoretiska Gymnasium" },
     { year: 2011, description: "Finnished upper secondary school" },
     { year: 2012, description: "Skied & snowboarded in Whistler, Canada for 6 months." },
-    { year: 2013, description: "Backpacked through South America for four months with a friend and started Engineering preparatory year at KTH (Royal Institute of Technology" },
+    { year: 2013, description: "Backpacked through South America for four months with a friend and started Engineering preparatory year at KTH (Royal Institute of Technology)" },
     { year: 2014, description: "Started studying Industrial engineering & management at the Faculty of Engineering, Lund University" },
     { year: 2020, description: "Almost finnished with my master, I decided to create this portfolio." }
   ]
@@ -26,7 +26,6 @@ export class TimelineComponent implements OnInit {
 
   setEvent(event:Event){
     this.currentEvent = event;
-    console.log(event);
   }
 
 
